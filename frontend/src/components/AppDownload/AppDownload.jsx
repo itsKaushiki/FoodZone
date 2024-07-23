11564f9da67f9285193d84e9604312b7d0e7e2c4
itsKaushiki <2022ugec032@nitjsr.ac.in>
2024-07-24 01:33:24 +0530

    updated

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import './AppDownload.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './AppDownload.css';
+import { assets } from '../../assets/assets';
 
 const AppDownload = () => {
     return (
         <div className='app-download' id='app-download'>
-            <p>For Better Experience Download <br />Tomato App</p>
+            <p>For a Better Experience, Download <br />the FoodZone App</p>
             <div className="app-download-platforms">
-                <img src={assets.play_store} alt="" />
-                <img src={assets.app_store} alt="" />
+                <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+                    <img src={assets.play_store} alt="Download from Play Store" />
+                </a>
+                <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+                    <img src={assets.app_store} alt="Download from App Store" />
+                </a>
             </div>
         </div>
-    )
+    );
 }
 
-export default AppDownload
+export default AppDownload;
